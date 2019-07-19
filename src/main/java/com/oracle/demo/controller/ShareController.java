b9632c8f699a3f3b5409859fddb02d1614298a36
zty <1036440150@qq.com>
2019-07-19 16:30:32 +0800

    7.19第二次zty

--- src/main/java/com/oracle/demo/controller/ShareController.java
+++ src/main/java/com/oracle/demo/controller/ShareController.java
@@ -6,12 +6,14 @@ import com.oracle.demo.entity.ShareInfo;
 import com.oracle.demo.entity.SharePicture;
 import com.oracle.demo.service.impl.ShareServiceImpl;
 import com.oracle.demo.util.FileUtils;
+import com.oracle.demo.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +44,7 @@ public class ShareController {
     }
 
     @RequestMapping("sendshare")
-    public String sendshare(@ModelAttribute Share share, MultipartFile file,HttpServletResponse response,
+    public String sendshare(@ModelAttribute Share share,@RequestParam("shareimg") MultipartFile file,HttpServletResponse response,
                             SharePicture picture, HttpSession httpSession) throws IOException
     {
 
@@ -51,18 +53,30 @@ public class ShareController {
         /**
          * 上传图片
          */
-        //图片上传成功后，将图片的地址写到数据库
-        String filePath = "D:/mysql_picture/";//保存图片的路径,tomcat中有配置
-        //获取原始图片的拓展名
-        String originalFilename = file.getOriginalFilename();
-        //新的文件名字，使用uuid随机生成数+原始图片名字，这样不会重复
-        String newFileName = UUID.randomUUID()+originalFilename;
-        //封装上传文件位置的全路径，就是硬盘路径+文件名
-        File targetFile = new File(newFileName);
-        //把本地文件上传到已经封装好的文件位置的全路径就是上面的targetFile
-        file.transferTo(targetFile);
-        picture.setImg(newFileName);//文件名保存到实体类对应属性上
-        picture.setShareId(share.getId());
+        String fileName= MD5Util.encode(share.getId()+"")+file.getOriginalFilename();
+        //相对地址
+        String filePath= ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static/shareimg/";
+        File dest=new File(filePath + fileName);
+        try{
+            file.transferTo(dest);
+            picture.setImg(fileName);//文件名保存到实体类对应属性上
+            picture.setShareId(share.getId());
+            shareService.savePicture(picture);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+//        //图片上传成功后，将图片的地址写到数据库
+//        String filePath = "D:/mysql_picture/";//保存图片的路径,tomcat中有配置
+//        //获取原始图片的拓展名
+//        String originalFilename = file.getOriginalFilename();
+//        //新的文件名字，使用uuid随机生成数+原始图片名字，这样不会重复
+//        String newFileName = UUID.randomUUID()+originalFilename;
+//        //封装上传文件位置的全路径，就是硬盘路径+文件名
+//        File targetFile = new File(newFileName);
+//        //把本地文件上传到已经封装好的文件位置的全路径就是上面的targetFile
+//        file.transferTo(targetFile);
+//        picture.setImg(newFileName);//文件名保存到实体类对应属性上
+//        picture.setShareId(share.getId());
 
         /**
          * 保存图片
@@ -73,38 +87,7 @@ public class ShareController {
         return "redirect:index";//需要跳转至动态首页控制器
     }
 
-    @Value("${web.upload-path}")
-    private String path;
-    /**
-     *
-     * @param file 要上传的文件
-     * @return
-     */
-    @RequestMapping("fileUpload")
-    public String upload(@ModelAttribute Share share, MultipartFile file,HttpServletResponse response,
-                         SharePicture picture, HttpSession httpSession) throws IOException{
 
-        /**
-         * 上传图片
-         */
-        //图片上传成功后，将图片的地址写到数据库
-        String filePath = "D:/mysql_picture/";//保存图片的路径,tomcat中有配置
-        //获取原始图片的拓展名
-        String originalFilename = file.getOriginalFilename();
-        //新的文件名字，使用uuid随机生成数+原始图片名字，这样不会重复
-        String newFileName = UUID.randomUUID()+originalFilename;
-        //封装上传文件位置的全路径，就是硬盘路径+文件名
-        File targetFile = new File(newFileName);
-        //把本地文件上传到已经封装好的文件位置的全路径就是上面的targetFile
-        file.transferTo(targetFile);
-        picture.setImg(newFileName);//文件名保存到实体类对应属性上
-        //picture.setShareId(share.getId());
-
-
-        shareService.savePicture(picture);
-
-        return "forward:/test";
-    }
 
     private final ResourceLoader resourceLoader;
    // @Value("${web.upload-path}")
@@ -125,15 +108,15 @@ public class ShareController {
      * 显示单张图片
      * @return
      */
-    @RequestMapping("{fileName}")
-    public ResponseEntity showPhotos(String fileName){
-
-        try {
-            // 由于是读取本机的文件，file是一定要加上的， path是在application配置文件中的路径
-            return ResponseEntity.ok(resourceLoader.getResource("file:" + path + fileName));
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
+//    @RequestMapping("/show")
+//    public ResponseEntity showPhotos(String fileName){
+//
+//        try {
+//            // 由于是读取本机的文件，file是一定要加上的， path是在application配置文件中的路径
+//            return ResponseEntity.ok(resourceLoader.getResource("file:" + path + fileName));
+//        } catch (Exception e) {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
 
 }
